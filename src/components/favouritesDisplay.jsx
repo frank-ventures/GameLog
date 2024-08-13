11432369dced0afdde3368511fcba2d149efcb5a
frank-ventures <6458936+frank-ventures@users.noreply.github.com>
2024-08-13 16:54:56 +0100

    Profile Favourites back to Client component, so that they refresh when new fave is added.

--- src/components/favouritesDisplay.jsx
+++ src/components/favouritesDisplay.jsx
@@ -1,3 +1,4 @@
+"use client";
 // React and Next
 // import { useContext, useEffect, useState } from "react";
 import Link from "next/link";
@@ -9,34 +10,42 @@ import NewLogWrapper from "./NewLogWrapper.jsx";
 import { Suspense } from "react";
 import QuantumSpinner from "./ldrsSpinners";
 
-export default function FavouritesDisplay({ fave, UserID }) {
-  console.log("FavouritesDisplay logging fave: ", fave);
+export default function FavouritesDisplay({ UserFaves, UserID }) {
+  // console.log("FavouritesDisplay logging UserFaves: ", UserFaves);
 
   return (
     <>
       <div className="user-favourites flex flex-col gap-2 mx-4 border border-gray-400">
-        {fave ? (
-          <div
-            className="individual-favourite flex flex-col border-b-[1px] border-gray-600"
-            key={fave.id}
-          >
-            <div className="game-fave-title flex flex-col w-full justify-center items-center p-2">
-              <h3 className="text-center">
-                <Link href={`/games/${fave.game_slug}`}>{fave.game_name}</Link>
-              </h3>
-              {/* TODO: MAKE IT SO THAT THIS WORKS ON THE PROFILE PAGE */}
-              {/* <Suspense fallback={<QuantumSpinner />}>
-                <ToggleFavouriteGameButton
-                  GameID={fave.igdbgame_id}
-                  GameName={fave.game_name}
-                />
-              </Suspense> */}
-            </div>
-            <div className="game-fave-log flex flex-col  gap-2 w-full p-2">
-              <h3>Logs</h3>
-              <NewLogWrapper UserID={UserID} IGDBGameID={fave.igdb_game_id} />
-            </div>
-          </div>
+        {UserFaves ? (
+          UserFaves.map((fave) => {
+            return (
+              <div
+                className="individual-favourite flex flex-col border-b-[1px] border-gray-600"
+                key={fave.id}
+              >
+                <div className="game-fave-title flex flex-col w-full justify-center items-center p-2">
+                  <h3 className="text-center">
+                    <Link href={`/games/${fave.game_slug}`}>
+                      {fave.game_name}
+                    </Link>
+                  </h3>
+                  <Suspense fallback={<QuantumSpinner />}>
+                    <ToggleFavouriteGameButton
+                      GameID={fave.igdb_game_id}
+                      GameName={fave.game_name}
+                    />
+                  </Suspense>
+                </div>
+                <div className="game-fave-log flex flex-col  gap-2 w-full p-2">
+                  <h3>Logs</h3>
+                  <NewLogWrapper
+                    UserID={UserID}
+                    IGDBGameID={fave.igdb_game_id}
+                  />
+                </div>
+              </div>
+            );
+          })
         ) : (
           <p>uh oh. Something went wrong. Tell Frank</p>
         )}
